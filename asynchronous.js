b2211662f0901a2501ac54269f0e0d9832edd4bb
Axumawityemaneh <yemanehaxumawit@gmail.com>
2024-05-17 23:36:58 +0300

    commit changes

--- asynchronous.js
+++ asynchronous.js
@@ -1,13 +1,18 @@
-//Question one
+//Question one  Write an asynchronous function that accepts
+// a message string and a delay time in milliseconds. 
+//The function should log the message to the console after the specified delay time.
 async function acceptMesageWithDelay(message,delayMessage){
-    await new Promise((resolve) =>{
+    await new Promise((resolve,reject) =>{
 setTimeout(resolve,delayMessage)});
 console.log(message)
 }
 acceptMesageWithDelay("hello,world",2000);
 
 
-//Question two
+
+//Question two  You have an array of user IDs and a function getUserData(id)
+// that returns a Promise with user data when given a user ID. Write an asynchronous function
+// that fetches and logs the data for each user ID one by one, in sequence.
 const getUserData = id => {
     // Simulate async API call
     return new Promise(resolve => {
@@ -25,7 +30,12 @@ const getUserData = id => {
   const userIds = [1, 2, 3];
   logUserData(userIds);
 
-//Question three
+
+
+//Question three You have an asynchronous function performTask() that returns a Promise.
+ //The Promise resolves if the task is successful and rejects if 
+ //there's an error. Write a function that calls performTask() and logs a custom success message 
+ //if the task is successful, and a custom error message if there's an error.
 async function handleTask(){
     try{
         await performTask();
@@ -40,7 +50,21 @@ async function handleTask(){
     }
 }
 
-  //Question four
+  //Question four   
+ // Retry Logic:
+//Scenario:
+//Write a function unstableTask that:
+
+//1.Accepts a taskName and failureProbability (a number between 0 and 1).
+//2. Returns a Promise that:
+//Resolves immediately with a success message if a randomly generated number is greater than failureProbability.
+//Rejects immediately with a failure message if a randomly generated number is less than or equal to failureProbability.
+//Write another function executeWithRetry that:
+//Accepts a taskName, retries, and failureProbability.
+//Uses a retry mechanism to attempt the unstableTask up to retries times.
+//Logs whether the task succeeded or failed after all attempts.
+
+
   function unstableTask(taskName, failureProbability) {
     return new Promise((resolve, reject) => {
       const randomNumber = Math.random();
@@ -65,5 +89,6 @@ async function handleTask(){
   }
   executeWithRetry('Data Processing', 3, 0.3);
   
-    
+  
+  
   
